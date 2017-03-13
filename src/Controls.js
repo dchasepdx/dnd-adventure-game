c8221b770c478f0eeeb1d0d7cde21c95c588823b
David Chase <dchasepdx@gmail.com>
2017-03-13 12:49:27 -0700

    fixes problems with death tracker and orcs turn. the settimeout function that displayed the orcs turn would execute even if the orc had died during the previous turn. Uses timeout id to cancel the function.

--- src/Controls.js
+++ src/Controls.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default function Controls(props) {
-  let fight;
-  fight = <button onClick={props.combatRound}>fight!</button>;
   return (
   <div>
-    {fight}
+    {!props.orcsTurn && 
+      <button onClick={props.combatRound}>fight!</button>
+    }
   </div>
   );
 }
--- src/chars.js
+++ src/chars.js
@@ -10,7 +10,7 @@ export default {
     id: 'orc',
     ac: 10,
     atk: 1,
-    health: 12
+    health: 10
   }
 };
 
